e4d20b75c2e9bb25caaa70d56d9e46bab4526a04
nishant-b-bit <nishantb9822@gmail.com>
2026-05-27 16:48:17 +0545

    changed the code of login and signup function

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -2,25 +2,7 @@ from flask import render_template, request,redirect, url_for
 class AuthController:
  
     def login(self):
-        if request.method == "POST":
-            email = request.form.get("email")
-            password = request.form.get("password")
-
-            user = User.login_user(email, password)
-
-            if user:
-                if user["role"] == "student":
-                    return redirect(url_for("student.dashboard"))
-
-                elif user["role"] == "organizer":
-                    return redirect(url_for("organizer.dashboard"))
-
-                elif user["role"] == "admin":
-                    return redirect(url_for("admin.dashboard"))
-
-            return "Invalid Credentials"
-
-        return render_template("login.html")
+     return render_template("login.html")
  
     def register(self):
         if request.method == "POST":
@@ -43,16 +25,6 @@ class AuthController:
         return render_template("event_details.html")
  
     def signup(self):
-        if request.method == "POST":
-            name = request.form.get("name")
-            email = request.form.get("email")
-            password = request.form.get("password")
-            role = request.form.get("role")
-
-            User.create_user(name, email, password, role)
-
-            return "User registered successfully"
-
         return render_template("signup.html")
  
     def view_events(self):
