36899e15d0706759078fadd3582114188404c443
nishant-b-bit <nishantb9822@gmail.com>
2026-05-29 08:11:25 +0545

    updated the route of view events

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -15,4 +15,5 @@ class AuthRoutes:
         self.bp.route("/create_events",methods=["GET","POST"])(self.controller.create_events)
         self.bp.route("/organizer_dashboard", methods=["GET", "POST"])(self.controller.organizer_dashboard)
         self.bp.route("/register_event", methods=["GET", "POST"])(self.controller.register_event)
+        self.bp.route("/view_events", methods=["GET", "POST"])(self.controller.view_events)
         return self.bp
