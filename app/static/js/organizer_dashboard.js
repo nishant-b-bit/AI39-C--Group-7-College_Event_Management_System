fa3ccbf4a2bfaafd888058f436232e57d12a8a2e
nishant-b-bit <nishantb9822@gmail.com>
2026-05-29 07:35:24 +0545

    created css, html and js file for organizer dashboard

---
+++ app/static/js/organizer_dashboard.js
@@ -0,0 +1,19 @@
+const modal = document.getElementById("eventModal");
+
+const openBtn = document.getElementById("openModalBtn");
+
+const closeBtn = document.getElementById("closeModalBtn");
+
+openBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
