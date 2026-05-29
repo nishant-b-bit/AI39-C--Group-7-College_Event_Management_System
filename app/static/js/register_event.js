960d575308f331234a433aaf782c979cffdbdb1b
nishant-b-bit <nishantb9822@gmail.com>
2026-05-29 07:59:48 +0545

    created the css, js and html file for register event

---
+++ app/static/js/register_event.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("registerForm");
+
+form.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    alert("Registration submitted successfully!");
+
+});
