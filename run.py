bc61a89376951babec4a35b356862b1f316d5661
nishant-b-bit <nishantb9822@gmail.com>
2026-05-30 11:04:57 +0545

    added the code in the run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import create_app
 from app.routes.auth_routes import AuthRoutes
+from app.models.database import Database
 
 app = create_app()
+Database.create_tables()
 
 auth_route = AuthRoutes()
 app.register_blueprint(auth_route.register())
