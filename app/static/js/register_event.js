81a009756f27d1dbf326fd4ea13f9be92653921e
nishant-b-bit <nishantb9822@gmail.com>
2026-06-07 08:14:18 +0545

    updated the code of script

--- app/static/js/register_event.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("registerForm");
-
-form.addEventListener("submit", function(e) {
-
-    e.preventDefault();
-
-    alert("Registration submitted successfully!");
-
-});
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,6 +1,8 @@
 const menuIcon = document.getElementById("menuIcon");
 const navLinks = document.getElementById("navLinks");
 
-menuIcon.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
+if (menuIcon && navLinks) {
+    menuIcon.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
