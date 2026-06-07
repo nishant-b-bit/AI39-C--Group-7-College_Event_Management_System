2819e332c808fc41de7207bd8c210c2fda79b783
nishant-b-bit <nishantb9822@gmail.com>
2026-06-07 09:12:24 +0545

    managed the code

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-secret_ley='random-secret-key'
+secret_key = 'random-secret-key'  
 mysql_host = "localhost"
 mysql_user = "root"
 mysql_password = "21Apr2124"
 mysql_database = "event_management"
-SECRET_KEY="mysecretkey"
+SECRET_KEY = "mysecretkey"
